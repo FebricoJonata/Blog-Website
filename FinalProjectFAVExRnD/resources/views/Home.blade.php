a59c0386b9ffc62afa053b952f297fd16248d7a3
Febrico Jonata <riko9647@gmail.com>
2022-03-12 23:12:44 +0700

    add navbar -rico

--- FinalProjectFAVExRnD/resources/views/Home.blade.php
+++ FinalProjectFAVExRnD/resources/views/Home.blade.php
@@ -4,9 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Website</title>
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Home Page</title>
 </head>
 <body>
-    
+    <nav id="navbar">
+        <div class="logo">
+            <h2>Febrico Jonata</h2>
+        </div>
+        <ul class="nav-item">
+            <li><a href="/">Home</a></li>
+            <li><a href="/create/book">Create Book</a></li>
+            <li><a href="/read/book">Read Book</a></li>
+        </ul>
+            <a class="cta" href="/create/book"><button>Login</button></a>
+    </nav>
+
 </body>
 </html>
