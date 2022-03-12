642cc22cbe8074496e4c6c7cb97ceafbed19917a
Yuric Julias <yuricjulias@gmail.com>
2022-03-13 00:10:04 +0700

    add form in Login, add LoginStyle.css in public - Yuric

--- FinalProjectFAVExRnD/resources/views/Login.blade.php
+++ FinalProjectFAVExRnD/resources/views/Login.blade.php
@@ -2,11 +2,27 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <div class="LoginContainer">
+        <form action="">
+            <label for="username">Username</label>
+            <input type="text" name="username" id="">
+            <br>
+            <label for="password">Password</label>
+            <input type="password" name="password" id="">
+            <br>
+            
+            <input type="checkbox" name="Remember" id="">
+            <label for="remember">Remember me</label>
+            <br>
+            <button type="Submit">Login</button>
+            //Sementara gini dulu
+        </form>
+    </div>
 </body>
 </html>
