aa5d5746fdb58a6f4f19bc5621bf197ffd2c1f49
Nicholaslie51 <nicholienardi51@gmail.com>
2022-03-13 02:15:41 +0700

    Navbar and styling css - Nicholas

--- FinalProjectFAVExRnD/resources/views/Home.blade.php
+++ FinalProjectFAVExRnD/resources/views/Home.blade.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/css/navbar.css">
-    <title>Home Page</title>
+    <title>Home</title>
+    <link rel="stylesheet" href="././public/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@400;700&family=Secular+One&display=swap" rel="stylesheet">
 </head>
 <body>
     <nav id="navbar">
@@ -18,6 +21,29 @@
             <li><a href="/">Read Book</a></li>
         </ul>
             <a class="cta" href="/"><button>Login</button></a>
+    </nav>
+
+    <!-- Nicholas - nav bar , udah ada styling juga -->
+    <nav id="Navbar">
+        <div class="navbar-container">
+            <div class="navbar-left">
+                <img class="logo" src="./Asset/images-removebg-preview.png" alt="logo">
+                <h1>LOGO</h1>
+            </div>
+            <div class="navbar-center">
+                <a href="#Home">HOME</a>
+                <a href="#About">ABOUT</a>
+                <a href="#Articles">ARTICLES</a>
+                <a href="#ContactUs">CONTACT US</a>
+            </div>
+            <div class="navbar-right">
+                <img class="search" src="./Asset/magnifying-glass.png" alt="seacrh">
+            </div>
+            <div class="navbar-login">
+                <a class="login" href="">Log In</a>
+                <a class="signup" href="">Sign Up</a>
+            </div>
+        </div>
     </nav>
 
 </body>
