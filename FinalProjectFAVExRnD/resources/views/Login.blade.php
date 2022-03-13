43bbe95a0844b980d1d460a2feaadbe49b5e2625
Yuric Julias <yuricjulias@gmail.com>
2022-03-13 10:41:23 +0700

    remove div line 30 - Yuric

--- FinalProjectFAVExRnD/resources/views/Login.blade.php
+++ FinalProjectFAVExRnD/resources/views/Login.blade.php
@@ -27,14 +27,11 @@
                 <a href="#">Forgot Password</a>
             </div>
             
-            <div class="loginBox">
-                <button type="Submit" class="submitButton">Login</button>
-                    <div class="RememberBox">
-                        <input type="checkbox" name="RememberMe" id="">
-                        <label for="RememberMe">Remember Me</label>
-                    </div>
-            </div>
-            
+            <button type="Submit" class="submitButton">Login</button>
+                <div class="RememberBox">
+                    <input type="checkbox" name="RememberMe" id="">
+                    <label for="RememberMe">Remember Me</label>
+                </div>
         </form>
     </div>
 </body>
