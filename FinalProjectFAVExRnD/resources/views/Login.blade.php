310fd70234add86083890b3cd9eafbb71d556f17
Yuric Julias <yuricjulias@gmail.com>
2022-03-13 09:43:16 +0700

    add Login Form + Image Box - Yuric

--- FinalProjectFAVExRnD/resources/views/Login.blade.php
+++ FinalProjectFAVExRnD/resources/views/Login.blade.php
@@ -5,23 +5,36 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../../public/css/LoginStyle.css">
 </head>
 <body>
+    <!-- Yuric Julias, Login form + image box-->
     <div class="LoginContainer">
-        <form action="">
+        <div class="imageBox">
+            
+        </div>
+        <form action="" id="LoginBox">
+            <div class="titleForm">
+                <h1>WELCOME BACK</h1>
+            </div>
             <label for="username">Username</label>
-            <input type="text" name="username" id="">
+            <input type="text" name="username" id="" required>
             <br>
             <label for="password">Password</label>
-            <input type="password" name="password" id="">
-            <br>
+            <input type="password" name="password" id="" required>
             
-            <input type="checkbox" name="Remember" id="">
-            <label for="remember">Remember me</label>
-            <br>
-            <button type="Submit">Login</button>
-            //Sementara gini dulu
+            <div class="ForgotBox">
+                <a href="#">Forgot Password</a>
+            </div>
+            
+            <div class="loginBox">
+                <button type="Submit" class="submitButton">Login</button>
+                    <div class="RememberBox">
+                        <input type="checkbox" name="RememberMe" id="">
+                        <label for="RememberMe">Remember Me</label>
+                    </div>
+            </div>
+            
         </form>
     </div>
 </body>
