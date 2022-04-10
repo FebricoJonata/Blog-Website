57c6e90e96c6c0c222e425a3eb81c2ea205149a5
Nicholaslie51 <nicholienardi51@gmail.com>
2022-04-10 10:48:33 +0700

    dashbaord untuk tampilan general html & css done - nicho

---
+++ FinalProjectFAVExRnD/resources/views/DashboardGeneral.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="/css/dashboardgeneralstyle.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@400;700&family=Secular+One&display=swap" rel="stylesheet">
+</head>
+<body>
+    <nav id="Navbar">
+        <div class="navbar-container">
+            <div class="navbar-left">
+                <img class="logo" src="/Assets/images-removebg-preview.png" alt="logo">
+                <h1>LOGO</h1>
+            </div>
+            <div class="navbar-center">
+                <a href="#Home">HOME</a>
+                <a href="#About">ABOUT</a>
+                <a href="#Articles">ARTICLES</a>
+                <a href="#ContactUs">CONTACT US</a>
+            </div>
+            <div class="navbar-login">
+                <a class="login" href="">Log In</a>
+                <a class="signup" href="">Sign Up</a>
+            </div>
+        </div>
+        <hr>
+    </nav>
+
+    <!-- bagian dashboard -->
+    <section id="Register">
+        <div class="register-container">
+            <div class="register-left">
+                <img src="/Assets/ae5ae16a1f8bdad663c96a699d91e646-removebg-preview.png" alt="gambar-utama">
+                <p class="p2">Together we study</p>
+                <p>Together we build the better world</p>
+            </div>
+            <div class="register-right">
+                <div class="register-heading">
+                    <img class="bg" src="./Assets/Vector.png" alt="gambar-utama">
+                    <h1>StudBlog</h1>
+                    <p class="p1">Studly is a website that provide online learning. Now, Studly announce their new product StudBlog</p>
+                    <p class="p2">Let’s try it!</p>
+                </div>
+            </div>
+</body>
+</html>
