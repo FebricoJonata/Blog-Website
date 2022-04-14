d70267351a104d6de8766c461cf731cda2da5426
Yuric Julias <yuricjulias@gmail.com>
2022-04-14 16:51:29 +0700

    Add Contact Us html & css -Yuric

---
+++ FinalProjectFAVExRnD/resources/views/ContactUs.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@400;700&family=Secular+One&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/css/ContactUs.css">
+    <title>Document</title>
+</head>
+<body>
+    <div id="header">
+        <div class="header-container">
+            <div class="header-title">Contact Us</div>
+            <div class="header-desc">Leave Your Message <hr /></div>
+        </div>
+        
+    </div>
+    <div id="Form">
+        <div class="form-container">
+            <div class="name">
+                <label for="name">Full Name:</label>
+                <input type="text" name="name" id="name">
+            </div>
+            <div class="email">
+                <label for="email">Email:</label>
+                <input type="text" name="email" id="email">
+            </div>
+            <div class="message">
+                <label for="message">Message:</label>
+                <textarea name="message" id="message"   cols="30" rows="10"></textarea>
+            </div>
+            <div class="button">
+                <button type="submit">Send</button>
+            </div>
+        </div>
+
+
+    </div>
+</body>
+</html>
