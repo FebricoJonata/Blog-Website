6e7c9453533c89a22df1163844cb3903db0e0e94
Yuric Julias <yuricjulias@gmail.com>
2022-04-17 00:19:55 +0700

    Rename Assets Icon, Add Footer - Yuric

---
+++ FinalProjectFAVExRnD/resources/views/Footer.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@400;700&family=Secular+One&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/FinalProjectFAVExRnD/public/css/Footer.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet">
+
+    <title>Document</title>
+</head>
+<body>
+   <div class="largeBox">
+       <div class="outerFoot">
+           <div class="top-footer">
+               <div class="top-footer-top">
+                   <p>Studly</p>
+               </div>
+               <div class="top-footer-low">
+                    <div class="dest-icon">
+                        <img src="/FinalProjectFAVExRnD/public/Assets/Destination icon.png" alt="">
+                    </div>
+                    <div class="dest-description">
+                        <p>Flying Chicken Road 12A, <br>Bogor, Indonesia, 10859</p>
+                    </div>
+               </div>
+           </div>
+           <div class="low-footer">
+               <div class="low-footer-left">
+                   <div class="link-desc">
+                       <p>FIND US AT</p>
+                   </div>
+                   <div class="icon-box">
+                        <div class="left-icon">
+                            <div class="twitter">
+                                <img src="/FinalProjectFAVExRnD/public/Assets/Twitter icon.png" alt="">
+                                <p>@Studly_ID</p>
+                            </div>
+                            <div class="facebook">
+                                <img src="/FinalProjectFAVExRnD/public/Assets/Facebook icon.png" alt="">
+                                <p>Studly Indonesia</p>
+                            </div>
+                        </div>
+                        <div class="right-icon">
+                            <div class="website">
+                                <img src="/FinalProjectFAVExRnD/public/Assets/Ball icon.png" alt="">
+                                <p>Studly Indonesia</p>
+                            </div>
+                            <div class="instagram">
+                                <img src="/FinalProjectFAVExRnD/public/Assets/Instagram icon.png" alt="">
+                                <p>@Studly_id</p>
+                            </div>
+                        </div>
+                   </div>
+               </div>
+               <div class="low-footer-right">
+                   <img src="/FinalProjectFAVExRnD/public/Assets/Online course lettering.png" alt="">
+               </div>
+           </div>
+       </div>
+   </div>
+</body>
+</html>
